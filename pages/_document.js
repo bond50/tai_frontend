3fc9a83789d3eb756ed83d7632fc3c1c9d5731f8
bond50 <galavu10@gmail.com>
2022-04-17 22:28:48 +0300

    refactor

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
                     <link rel="stylesheet"
                           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
                     <link
-                        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+                        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i&display=optional"
                         rel="stylesheet"/>
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
                           rel="stylesheet"
--- pages/tags/index.js
+++ pages/tags/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from "../../components/layout";
 
+
 const Index = () => {
     return (
          <Layout simple title='All Blog tags'>
