2d8a5df963559a68854e58b3045408cdb4b317bf
bond50 <galavu10@gmail.com>
2022-04-18 08:50:08 +0300

    refactor

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,32 @@
 import '../styles/globals.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
-import Layout from "../components/layout";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import {useEffect} from "react";
+import React, {useEffect} from 'react'
+import Head from "next/head";
+import 'nprogress/nprogress.css';
+import NProgress from 'nprogress';
+import Router from "next/router";
+import Script from "next/script";
 
 
 function MyApp({Component, pageProps}) {
+
+
+    NProgress.configure({showSpinner: false});
+    Router.onRouteChangeStart = () => NProgress.start();
+    Router.onRouteChangeComplete = () => NProgress.done();
+    Router.onRouteChangeError = () => NProgress.done();
+
+    const returnHead = () => {
+        return <Head>
+            <meta charSet="UTF-8"/>
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
+        </Head>
+    }
 
     useEffect(() => {
         AOS.init(
@@ -15,7 +35,29 @@ function MyApp({Component, pageProps}) {
     },)
 
 
-    return <Component {...pageProps}/>
-}
+    return <>
+         {
+            process.env.PRODUCTION && <>
+                <Script
+                    strategy="lazyOnload"
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+                />
 
+                <Script strategy="lazyOnload">
+                    {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+                </Script>
+            </>
+        }
+        {returnHead()}
+        <Component {...pageProps} />
+    </>
+
+}
 export default MyApp
