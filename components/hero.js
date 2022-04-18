8e728671a638f7f1ecff3b7dcda1891e1fe4733c
bond50 <galavu10@gmail.com>
2022-04-18 17:04:52 +0300

    refactor

--- components/hero.js
+++ components/hero.js
@@ -41,14 +41,15 @@ function BlogCarousel() {
                 />
                 <div className="carousel-container ">
                     <div className="container">
-                        <h2 className="animate__animated animate__fadeInDown">Welcome to <span>Multi</span></h2>
-                        <p className="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid
-                            qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias
-                            dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus
-                            deleniti vel. Minus et tempore modi architecto.</p>
-                        <a href="#about"
-                           className="btn-get-started animate__animated animate__fadeInUp scrollto">Read
-                            More</a>
+                        <h2 className="animate__animated animate__fadeInDown"><span>{d.title.toLowerCase()}</span></h2>
+                         {renderHTML(d.excerpt)}
+
+
+                          <Link href={`/blogs/${d.slug}`}>
+                                <a className="btn-get-started animate__animated animate__fadeInUp">Read
+                                    More
+                                </a>
+                            </Link>
                     </div>
                 </div>
             </Carousel.Item>
