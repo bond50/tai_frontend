34eef49da07ef55c8dddc19477e8fea2e7ebae2a
bond50 <galavu10@gmail.com>
2022-04-19 17:50:04 +0300

    refactor

--- components/sidebar/LoadRecentBlogs.js
+++ components/sidebar/LoadRecentBlogs.js
@@ -1,21 +1,20 @@
 import classes from '../../styles/LoadRecent.module.css'
 import moment from "moment";
 import Link from "next/link";
-import {API} from "../../config";
-import Image from "../reusables/lazy/Image";
 
 const LoadRecentBlogs = ({blogs}) => {
     const recent = () => {
         return blogs && blogs.map((blog, index) => {
-            return <div className={`${classes.Item} clearfix`} key={index}>
-                <Image  className="img img-fluid" src={`${API}/blog/photo/${blog.slug}`} alt={blog.title} width={350} height={250}/>
-                <h4>
-                    <Link href={`/blogs/${blog.slug}`} key={index}>
-                        <a> {blog.title.toLowerCase()}</a>
-                    </Link>
-                </h4>
-                <div className={classes.Time}>
-                    {moment(blog.updatedAt).fromNow()}
+            return <div className={`${classes.Item}`} key={index}>
+                <div className={classes.wrapper}>
+                    <h4>
+                        <Link href={`/blogs/${blog.slug}`} key={index}>
+                            <a> {blog.title.toLowerCase()}</a>
+                        </Link>
+                    </h4>
+                    <div className={classes.Time}>
+                        {moment(blog.updatedAt).fromNow()}
+                    </div>
                 </div>
             </div>
         })
