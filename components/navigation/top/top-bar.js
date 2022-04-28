c8750cb8e08faa2b36a5d313c5f9a51442c76e6e
bond50 <galavu10@gmail.com>
2022-04-28 16:58:22 +0300

    refactor

--- components/navigation/top/top-bar.js
+++ components/navigation/top/top-bar.js
@@ -2,21 +2,18 @@ import React from 'react';
 import classes from './topbar.module.css'
 
 const TopBar = ({scrolled}) => {
-    let barClasses = [`d-flex align-items-center  fixed-top`];
-    if (scrolled) {
-        barClasses.push('scrolled')
-    }
+
 
     return (
-        <div className={` ${classes.topBar} ${barClasses.join(" ")}`}>
-            <div className="container d-flex align-items-center  justify-content-md-between">
-                <div className="align-items-center d-flex mx-4">
+        <div className={` ${classes.topBar} ${scrolled ? 'scrolled' : ''}`}>
+            <div className="container d-flex align-items-center  justify-content-between ">
+                <div className="align-items-center d-flex ">
                     <i className="bi bi-clock"/>
-                    <span style={{fontSize:'13px'}}>Daily, 7AM to 10PM</span>
+                    <span>Monday - Sunday, 7AM to 10PM</span>
                 </div>
                 <div className={`${classes.link}`}>
-                    <a className="bi-envelope-open-fill"
-                       href="mailto:info@tailifestyle.co.ke?cc=tailifestyle01@gmail.com" target="_blank"
+                    <i className="bi-envelope-open"/>
+                    <a href="mailto:info@tailifestyle.co.ke?cc=tailifestyle01@gmail.com" target="_blank"
                        rel="noopener noreferrer"><span>info@tailifestyle.co.ke</span>
                     </a>
                 </div>
