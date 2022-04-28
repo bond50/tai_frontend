27e787911066cabc62ef0b12e821dab80f5f64b5
bond50 <galavu10@gmail.com>
2022-04-28 18:54:55 +0300

    refactor

--- components/navigation/top/top-bar.js
+++ components/navigation/top/top-bar.js
@@ -2,22 +2,26 @@ import React from 'react';
 import classes from './topbar.module.css'
 
 const TopBar = ({scrolled}) => {
-    let barClasses = [`d-flex align-items-center text-align-center fixed-top`];
+    let barClasses = [`d-flex align-items-center fixed-top`];
     if (scrolled) {
         barClasses.push('scrolled')
     }
 
     return (
+
         <div className={` ${classes.topBar} ${barClasses.join(" ")}`}>
-            <div className="container d-flex align-items-center  justify-content-md-between">
-                <div className="align-items-center d-flex mx-4">
-                    <i className="bi bi-clock"/>
-                    <span style={{fontSize:'13px'}}>Monday - Sunday, 7AM to 10PM</span>
+            <div className="container d-flex justify-content-between">
+                <div className={classes.contactInfo}>
+                        <i className="bi bi-clock"/><span>Monday - Sunday, 7AM to 10PM</span>
+                        <i className="bi bi-envelope d-md-none"/>
+                        <a className='d-md-none' href="mailto:info@tailifestyle.co.ke?cc=tailifestyle01@gmail.com" target="_blank"
+                           rel="noopener noreferrer">info@tailifestyle.co.ke</a>
+
                 </div>
-                <div className={`${classes.link}`}>
+                <div className={`d-none d-md-flex ${classes.socialLinks} align-items-center `}>
                     <a className="bi-envelope-open-fill"
                        href="mailto:info@tailifestyle.co.ke?cc=tailifestyle01@gmail.com" target="_blank"
-                       rel="noopener noreferrer"><span>info@tailifestyle.co.ke</span>
+                       rel="noopener noreferrer"><span className='mx-1'>info@tailifestyle.co.ke</span>
                     </a>
                 </div>
             </div>
@@ -27,3 +31,4 @@ const TopBar = ({scrolled}) => {
 };
 
 export default TopBar;
+
