c164530e0fd83b67afebabcf2046ddcde71a509b
bond50 <galavu10@gmail.com>
2022-04-28 16:18:57 +0300

    refactor

--- components/navigation/top/top-bar.js
+++ components/navigation/top/top-bar.js
@@ -9,14 +9,16 @@ const TopBar = ({scrolled}) => {
 
     return (
         <div className={` ${classes.topBar} ${barClasses.join(" ")}`}>
-            <div className="container d-flex align-items-center justify-content-center justify-content-md-between">
-                <div className="align-items-center  d-flex">
-                    <i className="bi bi-clock"/> <span>Monday - Sunday, 7AM to 10PM</span>
+            <div className="container d-flex align-items-center  justify-content-md-between">
+                <div className="align-items-center d-flex mx-4">
+                    <i className="bi bi-clock"/>
+                    <span style={{fontSize:'13px'}}>Monday - Sunday, 7AM to 10PM</span>
                 </div>
-                <div className={`d-flex  align-items-center ${classes.link}`}>
+                <div className={`${classes.link}`}>
                     <a className="bi-envelope-open-fill"
                        href="mailto:info@tailifestyle.co.ke?cc=tailifestyle01@gmail.com" target="_blank"
-                       rel="noopener noreferrer"><span>info@tailifestyle.co.ke</span></a>
+                       rel="noopener noreferrer"><span>info@tailifestyle.co.ke</span>
+                    </a>
                 </div>
             </div>
         </div>
